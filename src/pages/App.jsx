32f4580d8f4567bafe6170208397c71c930c7ae6
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-11-20 00:44:23 +0400

    Added fetching of data from fake store api

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import '../styles/App.css';
 
 function App() {
+  const [productData, setProductData] = useState(null);
+
+  // fetch product data here
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/')
+      .then((res) => res.json())
+      .then((json) => {
+        setProductData(json);
+      });
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <div>React Shopping Cart Demo</div>{' '}
+      {/* {productData != null ? <div>{productData[0].title}</div> : null} */}
+      <div>React Shopping Cart Demo</div>
     </div>
   );
 }
